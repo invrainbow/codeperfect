5da7e4a81dbf4dd9a7f290bd842f8be9b3d57b48
Brandon Hsiao <brhs.again@gmail.com>
2021-09-05 13:40:07 -0700

    save

--- ui.hpp
+++ ui.hpp
@@ -25,58 +25,7 @@ struct Font {
     i32 height;
     float width;
 
-    bool init(u8* font_data, u32 font_size, int texture_id) {
-        height = font_size;
-        tex_size = (i32)pow(2.0f, (i32)log2(sqrt((float)height * height * 128)) + 1);
-
-        u8* atlas_data = (u8*)our_malloc(tex_size * tex_size);
-        if (atlas_data == NULL)
-            return false;
-        defer { our_free(atlas_data); };
-
-        stbtt_pack_context context;
-        if (!stbtt_PackBegin(&context, atlas_data, tex_size, tex_size, 0, 1, NULL)) {
-            error("stbtt_PackBegin failed");
-            return false;
-        }
-
-        // TODO: what does this do?
-        stbtt_PackSetOversampling(&context, 2, 2);
-
-        if (!stbtt_PackFontRange(&context, font_data, 0, (float)height, ' ', '~' - ' ' + 1, char_info)) {
-            error("stbtt_PackFontRange failed");
-            return false;
-        }
-
-        if (!stbtt_PackFontRange(&context, font_data, 0, (float)height, 0xfffd, 1, &char_info[_countof(char_info) - 1])) {
-            error("stbtt_PackFontRange failed");
-            return false;
-        }
-
-        stbtt_PackEnd(&context);
-
-        glActiveTexture(GL_TEXTURE0 + texture_id);
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex_size, tex_size, 0, GL_RED, GL_UNSIGNED_BYTE, atlas_data);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        stbtt_fontinfo font;
-        i32 unscaled_advance, junk;
-        float scale;
-
-        stbtt_InitFont(&font, font_data, 0);
-        stbtt_GetCodepointHMetrics(&font, 'A', &unscaled_advance, &junk);
-        stbtt_GetFontVMetrics(&font, &offset_y, NULL, NULL);
-
-        scale = stbtt_ScaleForPixelHeight(&font, (float)height);
-        width = unscaled_advance * scale;
-        offset_y = (int)((float)offset_y * scale);
-
-        return true;
-    }
+    bool init(u8* font_data, u32 font_size, int texture_id);
 };
 
 struct Vert {
