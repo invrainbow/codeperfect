d3b11a9f4f8396c83ce15c4d02530a2117923b7f
Brandon Hsiao <bh@codeperfect95.com>
2021-10-14 15:23:00 -0700

    save

--- gostuff/versions/versions.go
+++ gostuff/versions/versions.go
@@ -13,11 +13,11 @@ var ValidOSes = map[string]bool{
 }
 
 var AppHashes = map[string]string{
-	"darwin": "7668e2b9e3affccfb4034241bbf4fb0d25fd137916df20eb43d0b8f1801311e2",
-	"darwin_arm": "df8af20d90ca9cf5f1b0e1b32499eb7370d3065fcb9b7c9c449ee2ce22e82c9d",
+	"darwin": "0699914b53b68ec2736f496df8e2f8dc4fd2488b91ce42b73c54050dcc54a5e0",
+	"darwin_arm": "1844d039b8b937b5c6eec6bc2631f748f394abdfa95e7f8cecd70543f38cead2",
 }
 
 var VersionUpdateHashes = map[string]string{
-	"darwin": "30936d93ae0f1b62d69e987d7f47c706ecca362078253f6ca1877b1b63228330",
-	"darwin_arm": "4e9559ed724b1c418ce2d14ed4dc2dd7bcb4bd222ab55be97e42a0f789ddeb2f",
+	"darwin": "1509a1b16149d9eb7f94b28cdf72928b3c2b5bf588b331e07103907584c35643",
+	"darwin_arm": "eb684778795a625f04be0b5cbf5d6944bbe0401c00bfa27e95b206764bde20f1",
 }
