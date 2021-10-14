6479e4be50b044d0f56abc5f28071fd971dbf17a
Brandon Hsiao <brhs.again@gmail.com>
2021-10-14 15:17:06 -0700

    save

--- gostuff/versions/versions.go
+++ gostuff/versions/versions.go
@@ -1,6 +1,6 @@
 package versions
 
-const CurrentVersion = 1
+const CurrentVersion = 2
 
 type Version struct {
 	URL  string
@@ -10,23 +10,14 @@ type Version struct {
 var ValidOSes = map[string]bool{
 	"darwin":     true,
 	"darwin_arm": true,
-	// "windows": true,
 }
 
-var VersionAppHashes = map[string]map[int]string{
-	"darwin": {
-		1: "3b86a23f8e70f3769c71e39800f4f46f437c364d0ea16204d3a5252263a0edbc",
-	},
-	"darwin_arm": {
-		1: "9e948230bc5e66870e1b98dce23366fe73bc5e2c1846b13faa34600bc8eb1c10",
-	},
+var AppHashes = map[string]string{
+	"darwin": "7668e2b9e3affccfb4034241bbf4fb0d25fd137916df20eb43d0b8f1801311e2",
+	"darwin_arm": "9e948230bc5e66870e1b98dce23366fe73bc5e2c1846b13faa34600bc8eb1c10",
 }
 
-var VersionUpdateHashes = map[string]map[int]string{
-	"darwin": {
-		1: "75a615b27f95dc115a14de4a617a84655501c0c76ccba8894c80baefc9156f67",
-	},
-	"darwin_arm": {
-		1: "052bfd9ccca68b3727ad7b76a7a6077c512a4a977df40a006462d25110f2c77b",
-	},
+var VersionUpdateHashes = map[string]string{
+	"darwin": "30936d93ae0f1b62d69e987d7f47c706ecca362078253f6ca1877b1b63228330",
+	"darwin_arm": "052bfd9ccca68b3727ad7b76a7a6077c512a4a977df40a006462d25110f2c77b",
 }
