1b521d4077f18c2d27e7786c3bc3b445dbfffe00
Brandon Hsiao <bh@codeperfect95.com>
2021-10-28 00:23:14 -0700

    update versions

--- gostuff/versions/versions.go
+++ gostuff/versions/versions.go
@@ -1,6 +1,6 @@
 package versions
 
-const CurrentVersion = 2
+const CurrentVersion = 3
 
 type Version struct {
 	URL  string
@@ -13,11 +13,11 @@ var ValidOSes = map[string]bool{
 }
 
 var AppHashes = map[string]string{
-	"darwin": "0699914b53b68ec2736f496df8e2f8dc4fd2488b91ce42b73c54050dcc54a5e0",
-	"darwin_arm": "1844d039b8b937b5c6eec6bc2631f748f394abdfa95e7f8cecd70543f38cead2",
+	"darwin": "6979b473302eb1b3e4bfc6b99eb4f8afd536b3af8a3df298c735a8388e200890",
+	"darwin_arm": "544a34b99e016b8a3a31bdedfe4b01d73749383de4bfde0b9af4758caffebee0",
 }
 
 var VersionUpdateHashes = map[string]string{
-	"darwin": "1509a1b16149d9eb7f94b28cdf72928b3c2b5bf588b331e07103907584c35643",
-	"darwin_arm": "eb684778795a625f04be0b5cbf5d6944bbe0401c00bfa27e95b206764bde20f1",
+	"darwin": "8161181f29967bd00d47873fc43cef866473e6b44367f23eadc3fb160019611b",
+	"darwin_arm": "c2957f076b339ed151ab157f8772e744710ae8d958e43d30002ab1be227d94f4",
 }
