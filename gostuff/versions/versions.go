563bf9dd911cf8da997325c7e0e765c1c4cb47cc
Brandon Hsiao <brhs.again@gmail.com>
2021-11-24 18:25:54 -0800

    save

--- gostuff/versions/versions.go
+++ gostuff/versions/versions.go
@@ -13,11 +13,11 @@ var ValidOSes = map[string]bool{
 }
 
 var AppHashes = map[string]string{
-	"darwin":     "6979b473302eb1b3e4bfc6b99eb4f8afd536b3af8a3df298c735a8388e200890",
+	"darwin":     "86aa3fa6d6770cf7f5c41fa547a4704b9a1a26609312c8b6243a53ee4b53dd2e",
 	"darwin_arm": "43c823a333d1c581ee17fb0c44121773a5d0ff5ad990feb8374a8a7ee09f72c4",
 }
 
 var VersionUpdateHashes = map[string]string{
-	"darwin":     "8161181f29967bd00d47873fc43cef866473e6b44367f23eadc3fb160019611b",
+	"darwin":     "4eb0e7d919e87529f25a1a607957b3c4826d3dacb950a8d6b7bafdd4674fd354",
 	"darwin_arm": "ebdeb3e15e8583c91ece8f95a878a286273835c6e69b5a931202dbfa0a0b9467",
 }
