639a055fda80ede2b8a258e20f605bc9ba9169d8
Brandon Hsiao <bh@codeperfect95.com>
2021-11-26 22:13:13 -0800

    save

--- gostuff/versions/versions.go
+++ gostuff/versions/versions.go
@@ -14,10 +14,10 @@ var ValidOSes = map[string]bool{
 
 var AppHashes = map[string]string{
 	"darwin":     "cf54b4f207de85f1e217c9eac1d43ad48888dfc4f89141c5b933cc87f688c01d",
-	"darwin_arm": "43c823a333d1c581ee17fb0c44121773a5d0ff5ad990feb8374a8a7ee09f72c4",
+	"darwin_arm": "88a3e5ade369dba55045e9a921c749216dd948ddede29ccd9ac78bf4a9d67a8e",
 }
 
 var VersionUpdateHashes = map[string]string{
 	"darwin":     "fce925373ddd58415f76bb74e01dc89aff2766a083e1c4549009b43d4deeacce",
-	"darwin_arm": "ebdeb3e15e8583c91ece8f95a878a286273835c6e69b5a931202dbfa0a0b9467",
+	"darwin_arm": "28041ed2f9352f503ce37945ecf4c61dbf33689d2bd7181c3078e753ffc86f54",
 }
