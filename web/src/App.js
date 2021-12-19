f5d35d96efeef34bc822910f93e883ad40654635
Brandon Hsiao <bh@codeperfect95.com>
2021-12-18 23:50:19 -0800

    fix

--- web/src/App.js
+++ web/src/App.js
@@ -54,6 +54,12 @@ function A({ children, ...props }) {
 }
 
 function WallOfText({ width, children, className, ...props }) {
+  // for tailwindcss shake:
+  // max-w-0 max-w-none max-w-xs max-w-sm max-w-md max-w-lg max-w-xl max-w-2xl
+  // max-w-3xl max-w-4xl max-w-5xl max-w-6xl max-w-7xl max-w-full max-w-min max-w-max
+  // max-w-fit max-w-prose max-w-screen-sm max-w-screen-md max-w-screen-lg
+  // max-w-screen-xl max-w-screen-2xl
+
   return (
     <div
       className={cx(
