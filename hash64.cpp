d4ec55f0231125eb83ac8754854e7f3bf27625e5
Brandon Hsiao <bh@codeperfect95.com>
2022-02-12 02:58:40 -0800

    just use xxhash

--- hash64.cpp
+++ hash64.cpp
@@ -1,17 +1,7 @@
 #include "hash64.hpp"
-#include "os.hpp"
-
-#if OS_MAC && defined(CPU_ARM64)
-#   define XXH_INLINE_ALL
-#   include "xxhash.h"
-#else
-#   include "meow_hash_x64_aesni.h"
-#endif
+#define XXH_INLINE_ALL
+#include "xxhash.h"
 
 u64 hash64(void *data, s32 len) {
-#if OS_MAC && defined(CPU_ARM64)
     return XXH3_64bits(data, len);
-#else
-    return MeowU64From(MeowHash(MeowDefaultSeed, len, data), 0);
-#endif
 }
