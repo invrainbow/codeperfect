886d8a7578c7755c84503675b51905e4164fa99b
Brandon Hsiao <bh@codeperfect95.com>
2022-08-15 10:15:05 -0700

    update stuff

--- gostuff/versions/versions.go
+++ gostuff/versions/versions.go
@@ -1,3 +1,21 @@
 package versions
 
-const CurrentVersion = 6
+import (
+	"fmt"
+)
+
+const CurrentVersion = 220800
+
+func CurrentVersionAsString() string {
+	v := CurrentVersion
+
+	major := (v / 100) / 100
+	minor := (v / 100) % 100
+	patch := v % 100
+
+	ret := fmt.Sprintf("%02d.%02d", major, minor)
+	if patch > 0 {
+		ret = fmt.Sprintf("%s.%d", ret, patch)
+	}
+	return ret
+}
