8bb6cfa459021433321b963a271e73f38d7ffa7b
Brandon Hsiao <bh@codeperfect95.com>
2022-08-16 10:06:02 -0700

    ENG-197: add socials to website (and other things)

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -6,6 +6,8 @@ import { BsChevronRight } from "@react-icons/all-files/bs/BsChevronRight";
 import { BsCodeSlash } from "@react-icons/all-files/bs/BsCodeSlash";
 import { FaLayerGroup } from "@react-icons/all-files/fa/FaLayerGroup";
 import { FaLinux } from "@react-icons/all-files/fa/FaLinux";
+import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter";
+import { FaDiscord } from "@react-icons/all-files/fa/FaDiscord";
 import { FaRobot } from "@react-icons/all-files/fa/FaRobot";
 import { GoPackage } from "@react-icons/all-files/go/GoPackage";
 import { GrInfo } from "@react-icons/all-files/gr/GrInfo";
@@ -45,6 +47,8 @@ const LINKS = {
   buyPersonalYearly: "https://buy.stripe.com/fZefZb2aTdEAbi8aEN",
   buyProfessionalMonthly: "https://buy.stripe.com/6oE28ldTB5843PG9AK",
   buyProfessionalYearly: "https://buy.stripe.com/28o8wJ3eXfMI4TK5kv",
+  twitter: "https://twitter.com/codeperfect95",
+  discord: "https://discord.gg/WkFY44BY7a",
 };
 
 const CDN_PATH = "https://d2mje1xp79ofdv.cloudfront.net";
@@ -55,33 +59,21 @@ function asset(path) {
 }
 
 function A({ link, children, href, ...props }) {
-  if (link) {
-    return (
-      <Link to={href} {...props}>
-        {children}
-      </Link>
-    );
+  const ext =
+    !href || href.startsWith("http://") || href.startsWith("https://");
+  if (ext) {
+    props.href = href;
+    return <a {...props}>{children}</a>;
   }
-  return (
-    <a href={href} {...props}>
-      {children}
-    </a>
-  );
+  props.to = href;
+  return <Link {...props}>{children}</Link>;
 }
 
-function WallOfText({ width, children, className, ...props }) {
-  // for tailwindcss shake:
-  // md:max-w-0 md:max-w-none md:max-w-xs md:max-w-sm md:max-w-md md:max-w-lg
-  // md:max-w-xl md:max-w-2xl md:max-w-3xl md:max-w-4xl md:max-w-5xl md:max-w-6xl
-  // md:max-w-7xl md:max-w-full md:max-w-min md:max-w-max md:max-w-fit md:max-w-prose
-  // md:max-w-screen-sm md:max-w-screen-md md:max-w-screen-lg md:max-w-screen-xl
-  // md:max-w-screen-2xl
-
+function WallOfText({ children, className, ...props }) {
   return (
     <div
       className={cx(
-        "wall-of-text p-4 py-10 md:py-28 leading-normal md:mx-auto",
-        `md:max-w-${width || "2xl"}`,
+        "wall-of-text p-4 py-10 md:py-28 leading-normal md:mx-auto md:max-w-2xl",
         className
       )}
       {...props}
@@ -170,16 +162,16 @@ function YoutubeEmbed({ videoId, first }) {
   const autoplay = first ? 0 : 1;
   return (
     <div
-      className="relative h-0 rounded-lg border-2 border-black overflow-hidden shadow-md"
+      className="relative h-0 md:rounded-lg md:border-2 md:border-black overflow-hidden shadow-md"
       style={{ paddingBottom: "58.60%" }}
     >
       <iframe
         className="absolute t-0 l-0 w-full h-full"
         src={`https://www.youtube.com/embed/${videoId}?autoplay=${autoplay}&loop=1&playlist=${videoId}&showinfo=0&controls=1&modestbranding=1`}
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       />
     </div>
   );
@@ -193,13 +185,14 @@ function VideoPlayer() {
   return (
     <div
       className="md:grid w-full md:max-w-screen-xl mx-auto md:gap-x-6 text-sm"
-      style={{ "grid-template-columns": "250px auto" }}
+      style={{ gridTemplateColumns: "250px auto" }}
     >
       <div className="max-w-full md:max-w-xs">
         {FEATURES.map((it) => {
           const isSelected = selected && selected.name === it.name;
           return (
             <button
+              key={it.name}
               className={cx(
                 "text-left block w-full rounded-md relative mb-1 overflow-hidden",
                 isSelected ? "opacity-100" : "opacity-70"
@@ -259,10 +252,10 @@ function VideoPlayer() {
 
 function Home() {
   return (
-    <div className="my-8 md:my-20 w-full">
+    <div className="mt-8 mb-14 md:my-20 w-full">
       <div className="max-w-full md:max-w-xl md:mx-auto text-lg md:text-lg leading-relaxed mb-16 md:mb-24 p-4">
-        <div className="text-center font-bold text-3xl md:text-4xl mb-8 text-black">
-          A High Performance IDE for Go
+        <div className="text-center font-bold text-3xl md:text-4xl mb-8 text-black leading-snug">
+          A&nbsp;High&nbsp;Performance IDE&nbsp;for&nbsp;Go
         </div>
         <p>
           Written in C++, CodePerfect indexes large codebases quickly and
@@ -313,10 +306,7 @@ function PaymentDone() {
       </p>
       <p>
         If you haven't done so yet, you can download CodePerfect{" "}
-        <A link href="/download">
-          here
-        </A>
-        .
+        <A href="/download">here</A>.
       </p>
     </WallOfText>
   );
@@ -504,11 +494,8 @@ function Download() {
       <Title>Download CodePerfect</Title>
       <p>
         CodePerfect is free to evaluate for 7 days, with all features available.
-        After the trial period you'll need a{" "}
-        <A link href="/buy">
-          license
-        </A>{" "}
-        for continued use.
+        After the trial period you'll need a <A href="/buy">license</A> for
+        continued use.
       </p>
       <div className="p-4 my-6 rounded bg-gray-100 border border-gray-400">
         <div className="font-bold mb-3">Build {CURRENT_BUILD}</div>
@@ -597,90 +584,85 @@ function PricingPoint({ label, not }) {
   );
 }
 
-function Layout() {
+function NavA({ href, className, children }) {
   return (
-    <div>
-      <ScrollToTop />
+    <A
+      href={href}
+      className={cx(
+        "text-black no-underline whitespace-nowrap hidden md:inline-block",
+        className
+      )}
+    >
+      {children}
+    </A>
+  );
+}
 
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>CodePerfect 95</title>
-      </Helmet>
+function Header() {
+  return (
+    <div className="pt-4 lg:pt-8 px-4 pb-4 flex justify-between items-center w-full md:max-w-screen-lg md:mx-auto text-lg">
+      <A
+        href="/"
+        className="font-bold text-lg text-black no-underline whitespace-nowrap flex items-center"
+      >
+        <img
+          alt="logo"
+          className="w-auto h-12 inline-block mr-3"
+          src={asset("/logo.png")}
+        />
+        <span className="hidden md:inline-block">CodePerfect 95</span>
+      </A>
+      <div className="flex items-baseline space-x-6">
+        <NavA href={LINKS.docs}>Docs</NavA>
+        <NavA href={LINKS.changelog}>Changelog</NavA>
+        <NavA href={LINKS.discord}>Discord</NavA>
+        <NavA href="/buy">Buy</NavA>
+        <A href="/download" className="button main-button">
+          <Icon className="mr-2" icon={HiOutlineDownload} />
+          Download
+        </A>
+      </div>
+    </div>
+  );
+}
 
-      <div className="">
-        <div className="pt-4 lg:pt-8 px-4 pb-4 flex justify-between items-center w-full md:max-w-screen-lg md:mx-auto text-lg">
-          <A
-            link
-            href="/"
-            className="font-bold text-lg text-black no-underline whitespace-nowrap flex items-center"
-          >
-            <img
-              alt="logo"
-              className="w-auto h-12 inline-block mr-3"
-              src={asset("/logo.png")}
-            />
-            <span className="hidden md:inline-block">CodePerfect 95</span>
-          </A>
-          <div className="flex items-baseline space-x-6">
-            <A
-              href={LINKS.docs}
-              className="text-black no-underline whitespace-nowrap hidden md:inline-block"
-            >
-              Docs
-            </A>
-            <A
-              href={LINKS.changelog}
-              className="text-black no-underline whitespace-nowrap hidden md:inline-block"
-            >
-              Changelog
-            </A>
-            <A
-              link
-              href="/buy"
-              className="text-black no-underline whitespace-nowrap hidden md:inline-block"
-            >
-              Buy
-            </A>
-            <A link href="/download" className="button main-button">
-              <Icon className="mr-2" icon={HiOutlineDownload} />
-              Download
-            </A>
-          </div>
-        </div>
-        <div>
-          <Outlet />
-        </div>
-        <div
-          className={cx(
-            "px-4 pt-4 mb-8 lg:pt-8 lg:mb-12 flex flex-col md:flex-row md:items-start justify-center gap-x-8",
-            "mr-4 border-r-100 text-gray-500"
-          )}
-        >
+function FootSection({ children }) {
+  return <div className="flex flex-col gap-y-3 md:gap-y-3">{children}</div>;
+}
+
+function FootA({ href, className, children }) {
+  return (
+    <A href={href} className={cx("text-gray-500 no-underline", className)}>
+      {children}
+    </A>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="px-4 pt-6 lg:pt-8 mb-4 md:mb-12 border-t border-gray-100">
+      <div className="flex flex-col md:flex-row gap-y-4 md:gap-0 hmd:flex-row justify-between w-full md:max-w-screen-lg md:mx-auto items-start">
+        <div className="flex flex-col md:flex-row md:items-start gap-y-3 md:gap-x-14 text-gray-500 leading-none">
           <span>&copy; {new Date().getFullYear()} CodePerfect 95</span>
-          <A link href="/buy" className="text-gray-500 no-underline md:hidden">
-            Buy
+          <FootSection>
+            <FootA href="/buy">Buy License</FootA>
+            <FootA href="/download">Download</FootA>
+          </FootSection>
+          <FootSection>
+            <FootA href={LINKS.docs}>Docs</FootA>
+            <FootA href={LINKS.changelog}>Changelog</FootA>
+          </FootSection>
+          <FootSection>
+            <FootA href={`mailto:${SUPPORT_EMAIL}`}>Support</FootA>
+            <FootA href="/terms">Terms &amp; Privacy</FootA>
+          </FootSection>
+        </div>
+        <div className="flex gap-x-4 text-2xl">
+          <A href={LINKS.discord}>
+            <Icon block icon={FaDiscord} />
           </A>
-          <A
-            link
-            href="/download"
-            className="text-gray-500 no-underline md:hidden"
-          >
-            Download
-          </A>
-          <A className="text-gray-500 no-underline" href={LINKS.docs}>
-            Docs
-          </A>
-          <A className="text-gray-500 no-underline" href={LINKS.changelog}>
-            Changelog
-          </A>
-          <A
-            className="text-gray-500 no-underline"
-            href={`mailto:${SUPPORT_EMAIL}`}
-          >
-            Support
-          </A>
-          <A link href="/terms" className="text-gray-500 no-underline">
-            Terms &amp; Privacy
+          <A href={LINKS.twitter}>
+            <Icon block icon={FaTwitter} />
           </A>
         </div>
       </div>
@@ -688,22 +670,39 @@ function Layout() {
   );
 }
 
+function Layout() {
+  return (
+    <div>
+      <ScrollToTop />
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="download" element={<Download />} />
-          <Route path="buy" element={<BuyLicense />} />
-          <Route path="payment-done" element={<PaymentDone />} />
-          <Route path="portal-done" element={<PortalDone />} />
-          <Route path="terms" element={<Terms />} />
-          <Route path="privacy" element={<Navigate to="terms" />} />
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>CodePerfect 95</title>
+      </Helmet>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="download" element={<Download />} />
+            <Route path="buy" element={<BuyLicense />} />
+            <Route path="payment-done" element={<PaymentDone />} />
+            <Route path="portal-done" element={<PortalDone />} />
+            <Route path="terms" element={<Terms />} />
+            <Route path="privacy" element={<Navigate to="terms" />} />
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
