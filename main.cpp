ebe3d28cc266a162fa047485691b692d03215c0d
Brandon Hsiao <bh@codeperfect95.com>
2022-09-15 15:06:39 -0700

    disable passing ctrl-f to vim for windows

--- main.cpp
+++ main.cpp
@@ -687,7 +687,9 @@ void handle_window_event(Window_Event *it) {
             case CP_KEY_D:
             case CP_KEY_U:
             case CP_KEY_B:
+#if !OS_WIN
             case CP_KEY_F:
+#endif
                 if (world.use_nvim)
                     if (world.nvim.mode != VI_INSERT)
                         send_nvim_keys(cp_sprintf("<C-%c>", tolower((char)key)));
