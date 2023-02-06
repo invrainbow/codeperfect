8090c7e57b1d1bf6a466dc60d2d13b1468570828
Brandon Hsiao <bh@codeperfect95.com>
2023-02-05 17:27:25 -0800

    add profiling for main.cpp

--- main.cpp
+++ main.cpp
@@ -1251,7 +1251,8 @@ int realmain(int argc, char **argv) {
     SCOPED_MEM(&tmpmem);
 
     Timer t;
-    t.init();
+    t.init(NULL, false);
+    // t.always_log = true;
 
     init_platform_crap();
 
@@ -1281,11 +1282,13 @@ int realmain(int argc, char **argv) {
 
     auto init_glew = []() -> bool {
         glewExperimental = GL_TRUE;
+        Timer t; t.init("glewInit", false);
         auto err = glewInit();
         if (err != GLEW_OK) {
             error("unable to init GLEW: %s", glewGetErrorString(err));
             return false;
         }
+        t.total();
         return true;
     };
 
@@ -1547,6 +1550,8 @@ int realmain(int argc, char **argv) {
     // from here, if we want to modify a texture, just set the active texture
     // are we going to run out of texture units?
 
+    t.log("generate textures");
+
     {
         SCOPED_FRAME();
 
@@ -1560,6 +1565,8 @@ int realmain(int argc, char **argv) {
             world.ui.im_font_ui = io.Fonts->AddFontFromMemoryTTF(ui.base_ui_font->data->get_data(), ui.base_ui_font->data->get_len(), UI_FONT_SIZE, &config);
             cp_assert(world.ui.im_font_ui);
         }
+
+        t.log("load im_ui_font");
 
         {
             // merge font awesome into main font
@@ -1579,8 +1586,12 @@ int realmain(int argc, char **argv) {
             ImWchar icon_ranges[] = { ICON_MIN_MD, ICON_MAX_MD, 0 };
             io.Fonts->AddFontFromMemoryTTF(material_icons_regular_ttf, material_icons_regular_ttf_len, ICON_FONT_SIZE, &config, icon_ranges);
 
+            t.log("load material icons font");
+
             ImWchar icon_ranges2[] = { 0x21E7, 0x21E7+1, 0 };
             io.Fonts->AddFontFromMemoryTTF(open_sans_ttf, open_sans_ttf_len, UI_FONT_SIZE, &config, icon_ranges2);
+
+            t.log("load open sans font");
         }
 
         {
@@ -1589,9 +1600,13 @@ int realmain(int argc, char **argv) {
             config.OversampleV = 3;
             world.ui.im_font_mono = io.Fonts->AddFontFromMemoryTTF(ui.base_font->data->get_data(), ui.base_font->data->get_len(), CODE_FONT_SIZE);
             cp_assert(world.ui.im_font_mono);
+
+            t.log("load im_font_mono");
         }
 
         io.Fonts->Build();
+
+        t.log("build fonts");
 
         // init imgui texture
         u8* pixels;
@@ -1603,7 +1618,11 @@ int realmain(int argc, char **argv) {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+        t.log("load texture data");
     }
+
+    t.log("init fonts");
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -1717,6 +1736,8 @@ int realmain(int argc, char **argv) {
             world.window->hide();
         world.jblow_tests.ready = true;
     }
+
+    t.total();
 
     for (;; world.frame_index++) {
         if (world.window->should_close) {
