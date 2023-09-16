c9d4abfadfbda0504729acd63027af7bf05acdba
Brandon Hsiao <bh@codeperfect95.com>
2023-09-16 12:55:51 -0700

    remove unnecessary scripts

--- go/build_windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem do we need this shit?
-rem -ldflags "-s -w" -o
-
-go build -o ../gohelper.dll -buildmode=c-shared "github.com/codeperfect95/codeperfect/go/cmd/helper"
-copy /Y ..\gohelper.dll ..\bin\gohelper.dll
