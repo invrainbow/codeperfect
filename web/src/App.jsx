c24e2e4eb1208290f41a63e34c77a109606ee36d
Brandon Hsiao <bh@codeperfect95.com>
2023-09-16 13:20:54 -0700

    update copy

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -141,9 +141,9 @@ function Home() {
       <div className="max-w-[500px] mx-auto my-20">
         <p>
           CodePerfect was an experiment to try and build a faster IDE. It
-          eschews the modern tech stack, is written from scratch in C/C++ like a
-          video game. It starts instantly, runs at 144 FPS, and has a near-zero
-          latency experience.
+          eschews the modern tech stack and is instead written from scratch in
+          C/C++/OpenGL like a video game. It starts instantly, runs at 144 FPS,
+          and has a near-zero latency experience.
         </p>
         <p>
           It comes with native full-featured code intelligence and integrated
